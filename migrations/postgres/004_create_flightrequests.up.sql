788d44ac0dd270bb99ea0bc5dd0b9d3b45b659b7
dev1 <dev1@gmail.com>
2026-09-14 22:38:56 +0300

    change types

--- migrations/postgres/004_create_flightrequests.up.sql
+++ migrations/postgres/004_create_flightrequests.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE flight_requests(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id INT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    request_date TIMESTAMPTZ,
 
     CONSTRAINT fk_user
         FOREIGN KEY (user_id)
